21384876e3627c3e2dcf81701103820ae6eeee03
Gugo-le <rngustmd0719@naver.com>
2024-07-06 10:23:06 +0900

    texttoimage

---
+++ lib/diff.py
@@ -0,0 +1,14 @@
+import replicate
+from dotenv import load_dotenv
+from pprint import pprint
+
+load_dotenv()
+
+output = replicate.run(
+  "stability-ai/sdxl:39ed52f2a78e934b3ba6e2a89f5b1c712de7dfea535525255b1aa35c5565e08b",
+  input={"prompt": "studying students in library"}
+)
+
+pprint(output)
+
+# ['https://replicate.delivery/pbxt/VJyWBjIYgqqCCBEhpkCqdevTgAJbl4fg62aO4o9A0x85CgNSA/out-0.png']
