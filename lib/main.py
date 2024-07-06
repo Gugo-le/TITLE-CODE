27eb7371b7606fdaaab53b4908498d95cd342c6e
Gugo-le <rngustmd0719@naver.com>
2024-07-06 20:34:21 +0900

    load factors

---
+++ lib/main.py
@@ -0,0 +1,12 @@
+import torch
+from PIL import Image as PILImage
+from transformers import CLIPProcessor, CLIPModel
+from diffusers import StableDiffusionPipeline
+
+
+clip_model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32")
+clip_processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+stable_diffusion = StableDiffusionPipeline.from_pretrained("CompVis/stable-diffusion-v1-4")
+stable_diffusion.to(device)
