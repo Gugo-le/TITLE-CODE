910708d375bc5c10c5c1146bff2fba5ee844367e
Gugo-le <rngustmd0719@naver.com>
2024-07-06 10:45:57 +0900

    clip

---
+++ lib/ImageSimilarity/Clip.py
@@ -0,0 +1,18 @@
+import torch
+import clip
+from PIL import Image
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+model, preprocess = clip.load("ViT-B/32", device=device)
+
+image = preprocess(Image.open("assets/imgs/gpt.jpg")).unsqueeze(0).to(device) # 원하는 이미지 경로로 수정
+text = clip.tokenize(["a person", "searching", "chatgpt"]).to(device)
+
+with torch.no_grad():
+    image_features = model.encode_image(image)
+    text_features = model.encode_text(text)
+    
+    logits_per_image, logits_per_text = model(image, text)
+    probs = logits_per_image.softmax(dim=-1).cpu().numpy()
+
+print("Label probs:", probs) 
