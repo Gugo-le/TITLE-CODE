bdf20248dee027f6e425cec8e55044cfeb416052
Gugo-le <rngustmd0719@naver.com>
2024-07-08 12:34:54 +0900

    color noise

--- lib/test/diffusion.py
+++ lib/test/diffusion.py
@@ -5,3 +5,9 @@ import matplotlib.pyplot as plt
 image_path = 'assets/imgs/cat.jpg'
 image = cv2.imread(image_path)
 image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+def add_color_noise(img, noise_level):
+    noise = np.random.normal(0, noise_level, img.shape)
+    noisy_img = img + noise
+    noisy_img = np.clip(noisy_img, 0, 255).astype(np.uint8)
+    return noisy_img
